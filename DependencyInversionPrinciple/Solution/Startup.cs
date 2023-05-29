29884559c51caf553e014aecd559c739bd1eccaa
Natan <natanael.sa@gmail.com>
2023-05-29 18:00:50 -0300

    Hanlders

--- DependencyInversionPrinciple/Solution/Startup.cs
+++ DependencyInversionPrinciple/Solution/Startup.cs
@@ -16,6 +16,7 @@ public class Startup
     {
         ConfigureRepository(services);
         ConfigureService(services);
+        ConfigureHandlers(services);
         ConfigureUseCase(services);
     }
 
